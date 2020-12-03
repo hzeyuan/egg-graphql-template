8af39d08fb6da6a6aa0d000d0389efdfc41bd16c
hzy <599012428@qq.com>
2020-12-03 15:24:23 +0800

    h

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Command, flags } from '@oclif/command'
+import { Input } from '@oclif/parser/lib/args'
 
 class EggTypeGraphqlcli extends Command {
   static description = '一个egg模板生成工具'
@@ -6,16 +7,15 @@ class EggTypeGraphqlcli extends Command {
   static flags = {
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
-
     // 生成模板
-    force: flags.boolean({ char: 't', description: '生成egg ts通用模板' }),
+    template: flags.string({ char: 't', description: '生成egg ts通用模板' }),
   }
 
-  // static args = [{ name: 'file' }]
+  static args = [{ name: 'template' },]
 
   async run() {
-    const { args, flags } = this.parse(EggTypeGraphqlcli)
-    this.log(`${args.name}`);
+    const { flags, args } = this.parse(EggTypeGraphqlcli)
+    this.log(`${flags.template} -- ${args.template}`);
 
   }
 }
