0d2edc2f29b1dabf4bbcbb7307d69bca5f16c5e0
hzy <599012428@qq.com>
2020-12-14 17:22:48 +0800

    gen Query Mutation

--- schema/App2WxappLoginStrategy.ts
+++
@@ -1,9 +0,0 @@
-import { Field, Int, ObjectType,InputType,registerEnumType } from 'type-graphql';
-
-@ObjectType()
-export class App2WxappLoginStrategy {
-    @Field(type => Boolean)
-    ticketExchangeUserInfoNeedSecret?: boolean;
-    @Field(type => Int)
-    ticketExpriresAfter?: number;
-}
